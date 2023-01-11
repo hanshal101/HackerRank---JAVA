742ffccf9551f0f6f33b3cb0df7b8cdb8a4988bc
Hanshal Mehta <122217807+hanshal101@users.noreply.github.com>
2023-01-11 18:17:07 +0530

    Create Java-Stdin-and-Stdout-II.java

---
+++ Java-Stdin-and-Stdout-II.java
@@ -0,0 +1,26 @@
+/*--------------------------------------------------------------------------------------------------------------------------------
+This is the fourth problem in HackerRank (JAVA)
+This problems explains the integer, double , string , char and float functions.
+----------------------------------------------------------------------------------------------------------------------------------
+*/
+import java.util.Scanner;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+            
+      Scanner sc=new Scanner(System.in);
+      int x=sc.nextInt();
+      double y=sc.nextDouble();
+      sc.nextLine();
+      String s=sc.nextLine();
+      
+      //Printing all the inputs
+      
+      System.out.println("String: "+s);
+      System.out.println("Double: "+y);
+      System.out.println("Int: "+x);
+    }
+}
